c342a518696c298b360f044bddbcd06504131371
mihirsam <xenon.of.x@gmail.com>
2018-08-27 00:00:07 +0530

    UserRegistration complete

--- 24.08.2018/OnlineBankingSystem/main.c
+++ 24.08.2018/OnlineBankingSystem/main.c
@@ -1,39 +1,42 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>Mihir
+#include <stdlib.h>
 #include <string.h>
+
+const int Upper = 10000, Lower = 1000;
 
 struct User
 {
     int AccNo;
-    char UserName[20], Password[100], PasswordCheck[100], Name[20];
+    char UserName[20], Password[100], Name[20];
 };
 
-struct UserRegster(struct User NewUser)
+void UserRegster(struct User *NewUser)
 {
-    char UserName[20];
-
     printf("\n\nWELCOME TO SMIT BANK\n");
 
     printf("\nEnter Your Name : ");
-    gets(NewUser.UserName);
+    gets(NewUser->Name);
+    printf("Select Username : ");
+    gets(NewUser->UserName);
+    printf("Enter The Password : ");
+    gets(NewUser->Password);
 
-    return NewUser;
-    //strcpy(NewUser->UserName, UserName);
-    //scanf("%s", &NewUser.Name);
-    //printf("Set Your Username : ");
-    //scanf("%s", &NewUser.UserName);
+    NewUser->AccNo =  (rand() % (Upper - (Lower + 1))) + Lower;
+    printf("\n\nRegistration Has Been Complete!\nNew Account Number : %d", NewUser->AccNo);
 
-};
+}
 
-int main() {
+void UserDisplay(struct User NewUser)
+{
+    printf("\n\nUser Information\n");
+    printf("\nName : %s\nUserName : %s\nAccount Number : %d\n", NewUser.Name, NewUser.UserName, NewUser.AccNo);
+}
+
+int main()
+{
     struct User UserList[100];
-
     UserRegster(&UserList[1]);
-    printf("Name : %s", UserList[1].Name);
-    puts(UserList[1].Name);
-    printf(UserList[1].Name);
-    gets(UserList[1].Name);
-    printf(UserList[1].Name);
+    UserDisplay(UserList[1]);
     return 0;
 }
